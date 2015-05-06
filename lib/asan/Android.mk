de8a2eac9a1d0f5eee0197931ae11e8075bca79e
Pirama Arumuga Nainar <pirama@google.com>
2015-05-06 11:49:53 -0700

    Update aosp/master compiler-rt for rebase to r235153
    Change-Id: I7c900e78d263fe0f574369e8383ccac7e36f5c3b

--- lib/asan/Android.mk
+++ lib/asan/Android.mk
@@ -156,7 +156,7 @@ LOCAL_CFLAGS += $(asan_rtl_cflags)
 LOCAL_SRC_FILES := $(asan_rtl_files) $(asan_rtl_cxx_files)
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_SHARED_LIBRARIES := liblog libc libdl
-LOCAL_STATIC_LIBRARIES := libcompiler_rt
+LOCAL_STATIC_LIBRARIES := libcompiler_rt libubsan_standalone
 # MacOS toolchain is out-of-date and does not support -z global.
 # TODO: re-enable once the toolchain issue is fixed.
 ifneq ($(HOST_OS),darwin)
